9688d6c1209bbf08f0742f94e2c1ab56fc04f22a
Lmsilvano <75850731+Lmsilvano@users.noreply.github.com>
2022-07-21 22:33:34 -0300

    tests done
    .

--- 3d-thereejs-tests/main.js
+++ 3d-thereejs-tests/main.js
@@ -1,23 +1,74 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+const scene = new THREE.Scene();
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-setupCounter(document.querySelector('#counter'))
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const donutTexture = new THREE.TextureLoader().load('donut_texture.jpg');
+const material = new THREE.MeshStandardMaterial({ map: donutTexture, });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+scene.add(pointLight, ambientLight);
+
+//const lightHelper = new THREE.PointLightHelper(pointLight);
+//const gridHelper = new THREE.GridHelper(200, 50);
+//scene.add(lightHelper, gridHelper);
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xFFFFFF });
+  const star = new THREE.Mesh(geometry, material);
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(150).fill().forEach(addStar);
+
+
+const universeTexture = new THREE.TextureLoader().load('universe.jpg');
+
+scene.background = universeTexture;
+
+
+// Neptune
+
+const neptuneTexture = new THREE.TextureLoader().load('neptune.jpg');
+const neptune = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 24, 24),
+  new THREE.MeshStandardMaterial({
+    map: neptuneTexture,
+  })
+)
+scene.add(neptune);
+
+function animate() {
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+  neptune.rotation.y += 0.01;
+  controls.update()
+  renderer.render(scene, camera);
+}
+
+animate();
